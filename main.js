8ab464eaa7d8ef92b5b48b43e904b799cfdf3f69
Yitang Wang <yitangwang0@gmail.com>
2021-08-24 11:10:50 +0800

    false

--- main.js
+++ main.js
@@ -5,7 +5,11 @@ const app = Vue.createApp({
             image: './assets/images/socks_blue.jpg',
             inStock: true,
             details: ['50% cotton', '30% wool', '20% polyester'],
-            sizes: ['12', '13', '14']
+            sizes: ['S', 'M', 'L', 'XL'],
+            variants: [
+              { id: 2234, color: 'green'},
+              { id: 2235, color: 'pink'}
+            ]
         }
     }
 })
