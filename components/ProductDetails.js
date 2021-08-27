6f8f7fbc25970e54ba5ff5e8da7dcede0cd1f301
Yitang Wang <yitangwang0@gmail.com>
2021-08-27 11:09:54 +0800

    no color

--- components/ProductDetails.js
+++ components/ProductDetails.js
@@ -1,8 +1,13 @@
 app.component('product-details', {
   props: {
     details: {
-      type: Boolean,
+      type: Array,
       require: true
     }
   },
+  template:
+  /*html*/
+  `<ul>
+    <li v-for="detail in details">{{ detail }}</li>
+  </ul>`
 })
